a9368632e5cef1b3769c732c1eb4ceb39eafea26
sagufta006 <sagufta.khan@torontomu.ca>
2024-10-04 03:26:31 -0400

    added imagee

--- script.js
+++ script.js
@@ -38,3 +38,23 @@ window.onclick = function(event) {
 }
 }
 
+var img = document.createElement("img"); 
+ 
+img.src = "IMG_0364.jpg"; 
+var src = document.getElementById("x"); 
+img.style.width = "350px";
+img.style.height = "350px"; 
+ 
+var src =document.getElementById("x")
+src.appendChild(img);
+
+// Function to enlarge the image when clicked
+img.onclick = function() {
+    if (img.style.width === "350px") {
+        img.style.width = "600px";  // Enlarged size
+        img.style.height = "600px"; // Enlarged size
+    } else {
+        img.style.width = "350px";  // Default size when clicked again
+        img.style.height = "350px"; 
+    }
+};

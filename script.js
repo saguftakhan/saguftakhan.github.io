ec2c5542b9a06e9737e7e045a9367bc0b9d2f1fa
sagufta006 <sagufta.khan@torontomu.ca>
2024-10-03 22:07:06 -0400

    added mover and mout

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ if (person != null) {
     "Hello " + person + "! How are you today?";
 }
 
+
+
+
+
+
+
+function mOver(some_tag) {
+    some_tag.innerHTML = "Thank You";
+     some_tag.style.color = "brown";
+  }
+  function mOut(some_tag) {
+     some_tag.innerHTML = "Mouse Over Me";
+     some_tag.style.color = "red";
+  }

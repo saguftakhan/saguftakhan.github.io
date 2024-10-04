2c66c68f425dcad6a22a946c65e9e79ec58f245f
sagufta006 <sagufta.khan@torontomu.ca>
2024-10-03 21:40:02 -0400

    updated script

---
+++ script.js
@@ -0,0 +1,7 @@
+let person = prompt("Please enter your name", "Angelina Jolie");
+
+if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+}
+

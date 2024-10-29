bf9777793471864e0950a4ad2c685a8ba6a9cbae
sagufta006 <sagufta.khan@torontomu.ca>
2024-10-29 00:22:47 -0400

    added contact page

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // Prompt to type in name
 let person = prompt("Please enter your name", "Your Name");
-
 if (person != null) {
-    document.getElementById("demo").innerHTML =
-    "Hello " + person + "! How are you today?";
+    document.getElementById("demo").innerHTML = "Hello " + person + "! How are you today?";
 }
 
 // Mouseover and Mouseout for menu items
-let menuItems = document.querySelectorAll('.navbar a');
+let menuItems = document.querySelectorAll('.navbar-container nav ul li a');
 menuItems.forEach(item => {
     item.addEventListener('mouseover', () => {
         item.style.color = 'black'; 
